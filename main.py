9e203fee0573537a33cdf4bb8ee9ba9bf88141e6
Pavel Mironchyk <pavel.mironchyk@gmail.com>
2018-04-06 13:45:26 +0200

    Adding websocket example

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-def main():
-    print('hello world!')
+import asyncio
+import datetime
+import random
+import websockets
+import json
+
+async def send_data(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        obj = {'now': now}
+        await websocket.send(json.dumps(obj))
+        await asyncio.sleep(random.random() * 3)
+
+start_server = websockets.serve(send_data, '127.0.0.1', 5678)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
