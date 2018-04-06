146aef32690b5b11494bef1d20588876b1e797a6
Pavel Mironchyk <pavel.mironchyk@gmail.com>
2018-04-06 14:55:21 +0200

    Adding some testing code

---
+++ simpleserver.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+
+PORT = 7000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
